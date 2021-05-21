9250560487b9dfd512b869a7fb7d1a841f6b83de
0xbug <clinlee.me@gmail.com>
2021-05-21 16:42:52 +0800

    upx 压缩体积

--- releases.sh
+++ releases.sh
@@ -1,3 +1,4 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w -s' -gcflags '-N -l' -o bin/biu-cli_linux_amd64 cmd/biu-cli.go
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags '-w -s' -gcflags '-N -l' -o bin/biu-cli_win_amd64.exe cmd/biu-cli.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags '-w -s' -gcflags '-N -l' -o bin/biu-cli_darwin_amd64 cmd/biu-cli.go
+upx bin/*
